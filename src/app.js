c0a63fb46ded53444d31932c3a0412fe36b2681a
Nishant-Sharma26 <nishusharma1817@gmail.com>
2025-03-03 22:31:57 +0530

    exploring get post put requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 
 const app = express();
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Nishant",lastName:"Sharma"})
+});
+
+app.put("/user",(req,res)=>{
+    res.send("data saved to database")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully")
+});
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Nishant",lastName:"Sharma"})
+});
 app.use("/test",(req,res)=>{
     res.send("hello from server");
 })
@@ -9,9 +25,11 @@ app.use("/home",(req,res)=>{
     res.send("hello hello helloo!");
 })
 
-app.use("/tes",(req,res)=>{
+app.use("/",(req,res)=>{
     res.send("hii my name is nishant");
 })
+
+
 app.listen(3000,()=>{
     console.log("server is successfully on the port 3000")
 });
