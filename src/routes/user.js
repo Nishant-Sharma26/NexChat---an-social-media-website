14f4ed647376e5eff7df263a648d2acb4ffaa2f3
Nishant-Sharma26 <nishusharma1817@gmail.com>
2025-03-13 14:14:53 +0530

    improve the feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -49,7 +49,8 @@ UserRouter.get("/feed",userAuth,async(req,res)=>{
        const loggedInUser = req.user;
         
        const page = parseInt(req.query.page)||1;
-       const limit = parseInt(req.query.limit)||10;
+       let limit = parseInt(req.query.limit)||10;
+       limit = limit > 50 ? 50 : limit;
        const skip = (page-1)*limit;
        const connectionRequests = await ConnectionRequestModel.find({
         $or:[
